f032654eb9bb6b1233b5e43677702508524813b2
Sneha Burman <snehaburman24@gmail.com>
2024-03-25 02:29:24 +0530

    First set up

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements 
+    '''
+    requirements=[]
+
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('\n', "") for req in requirements]
+
+    return requirements
+
+
+
+
+setup(
+name = 'MachineLearningProject', 
+version = '0.0.1',
+author = 'Sneha',
+author_email = 'snehaburman24@gmail.com',
+install_requires = get_requirements('requirements.txt')
+)
